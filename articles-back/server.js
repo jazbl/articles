e2f68f64e58947b2f7bf39dd91ed3cddd3575343
jasmine-ball <jazzie82@gmail.com>
2023-05-10 15:24:12 +1000

    Update server.js

--- articles-back/server.js
+++ articles-back/server.js
@@ -8,4 +8,4 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({"data":[{"id":1,"name":"Bad Janet"}]}));
-})
+});
