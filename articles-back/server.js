6e822c070ff4f933b593692508658a4523d82467
jasmine-ball <jazzie82@gmail.com>
2023-05-10 15:30:39 +1000

    Update server.js

--- articles-back/server.js
+++ articles-back/server.js
@@ -1,4 +1,4 @@
-// initial server.js file
+// jazbl
 
 const http = require('http');
 const hostname = '127.0.0.1';
@@ -8,4 +8,8 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({"data":[{"id":1,"name":"Bad Janet"}]}));
+});
+
+server.listen(port, hostname, () => {
+    console.log("Started")
 });
