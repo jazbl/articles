3aca24ac4072851bea0e075694a2478010049746
jasmine-ball <jazzie82@gmail.com>
2023-05-10 15:22:42 +1000

    Update server.js

--- articles-back/server.js
+++ articles-back/server.js
@@ -1,3 +1,11 @@
 // initial server.js file
 
 const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({"data":[{"id":1,"name":"Bad Janet"}]}));
+})
