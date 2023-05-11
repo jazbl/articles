124f872720e13c6e8ae3b3e6068c983e6d79c91d
jasmine-ball <jazzie82@gmail.com>
2023-05-11 18:38:56 +1000

    Update port number

--- articles-back/server.js
+++ articles-back/server.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 5001;
 
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
